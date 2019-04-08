bdfba6fb5387b75fa1be75efee98227d5e8c315d
alviraptri <43361781+alviraptri@users.noreply.github.com>
2019-04-08 11:30:22 +0700

    Ubah strings.xml
    slakdjasldjasldkja

--- app/src/main/java/com/example/alviraputri/ohrecipee/MainActivity.java
+++ app/src/main/java/com/example/alviraputri/ohrecipee/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button regist;
+    Button regist, login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +15,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         regist = (Button) findViewById(R.id.register);
+        login = (Button) findViewById(R.id.login);
 
         regist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent a = new Intent(getApplicationContext(), Main2Activity.class);
                 startActivity(a);
+                finish();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent b = new Intent(getApplicationContext(), home.class);
+                startActivity(b);
                 finish();
             }
         });
